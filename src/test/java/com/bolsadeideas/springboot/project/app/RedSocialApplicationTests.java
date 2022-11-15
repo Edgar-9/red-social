bd3f0ab5686de43ac2e8e4c46fca4babbccf1676
dylan29jf <dylanjay153@gmail.com>
2022-11-15 16:57:50 -0600

    v 1.0

---
+++ src/test/java/com/bolsadeideas/springboot/project/app/RedSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.project.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
